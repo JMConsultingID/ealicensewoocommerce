70e15120f80930dd77eb3af9d6b88246ee4fad60
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-09-06 00:17:39 +0800

    Update ealicensewoocommerce_function_public_woocommerce_email.php

--- inc/public/ealicensewoocommerce_function_public_woocommerce_email.php
+++ inc/public/ealicensewoocommerce_function_public_woocommerce_email.php
@@ -22,6 +22,8 @@ function ealicensewoocommerce_add_license_info_to_email($order, $sent_to_admin, 
         $license_key = get_post_meta($order->get_id(), '_ealicensewoocommerce_license_key', true);
         $account_quota = get_post_meta($order->get_id(), '_ealicensewoocommerce_account_quota', true);
         $license_expiration = get_post_meta($order->get_id(), '_ealicensewoocommerce_license_expiration', true);
+        $user_email = get_post_meta($order->get_id(), '_ealicensewoocommerce_email', true);
+        $user_password = get_post_meta($order->get_id(), '_ealicensewoocommerce_password', true);
 
 
         if ($license_key) {
@@ -30,12 +32,16 @@ function ealicensewoocommerce_add_license_info_to_email($order, $sent_to_admin, 
                 echo "License Key: " . $license_key . "\n";
                 echo "Account Limit: " . $account_quota . " Accounts\n";
                 echo "License Expiration: " . $license_expiration . "\n";
+                echo "License Expiration: " . $license_expiration . "\n";
+                echo "License Expiration: " . $license_expiration . "\n";
                 echo "Download your software here: " . esc_url($download_url) . "\n";
             } else {
                 // HTML email format
                 echo '<p><strong>' . __('License Key:', 'ealicensewoocommerce') . '</strong> ' . esc_html($license_key) . '</p>';
                 echo '<p><strong>' . __('Account Limit:', 'ealicensewoocommerce') . '</strong> ' . esc_html($account_quota) . ' Accounts</p>';
                 echo '<p><strong>' . __('License Expiration:', 'ealicensewoocommerce') . '</strong> ' . esc_html($license_expiration) . '</p>';
+                echo '<p><strong>' . __('User Email:', 'ealicensewoocommerce') . '</strong> ' . esc_html($user_email) . '</p>';
+                echo '<p><strong>' . __('User Password:', 'ealicensewoocommerce') . '</strong> ' . esc_html($user_password) . '</p>';
                 echo '<p><a href="' . esc_url($download_url) . '" target="_blank">' . __('Download your file here', 'ealicensewoocommerce') . '</a></p>';
             }
         }
