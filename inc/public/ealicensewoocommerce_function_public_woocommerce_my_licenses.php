8c2a662bb0d8415a1d5000ffb9e65bf5ac9036db
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-09-09 02:28:41 +0800

    Update ealicensewoocommerce_function_public_woocommerce_my_licenses.php

--- inc/public/ealicensewoocommerce_function_public_woocommerce_my_licenses.php
+++ inc/public/ealicensewoocommerce_function_public_woocommerce_my_licenses.php
@@ -74,7 +74,7 @@ function ealicensewoocommerce_display_licenses_by_email() {
         echo '<td>' . esc_html($license['account_quota']) . '</td>';
         echo '<td>' . esc_html($license['used_quota']) . '</td>';
         echo '<td>' . esc_html($license['license_expiration']) . '</td>';
-        echo '<td>' . esc_html($license['license_expiration_date'] ? date('Y-m-d', strtotime($license['license_creation_date'])) : 'Lifetime') . '</td>';
+        echo '<td>' . esc_html($license['license_expiration_date'] ? date('Y-m-d', strtotime($license['license_expiration_date'])) : 'Lifetime') . '</td>';
         echo '<td>' . esc_html($license['status']) . '</td>';
         echo '</tr>';
     }
