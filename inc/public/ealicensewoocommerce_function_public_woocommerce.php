1a0c642e150407d7c0fc401059507d401a573154
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-09-08 10:18:27 +0800

    update core

--- inc/public/ealicensewoocommerce_function_public_woocommerce.php
+++ inc/public/ealicensewoocommerce_function_public_woocommerce.php
@@ -58,12 +58,14 @@ function ealicensewoocommerce_display_license_info_on_thank_you_page($order_id) 
     // Check if the order exists and if the status is 'completed'
     if ($order && $order->get_status() === 'completed') {
         // Retrieve the license key from order meta
+        $user_email = get_post_meta($order->get_id(), '_ealicensewoocommerce_email', true);
         $license_key = get_post_meta($order->get_id(), '_ealicensewoocommerce_license_key', true);
         $account_quota = get_post_meta($order->get_id(), '_ealicensewoocommerce_account_quota', true);
         $license_expiration = get_post_meta($order->get_id(), '_ealicensewoocommerce_license_expiration', true);
 
         if ($license_key) {
             echo '<h2>' . __('Your License Details', 'ealicensewoocommerce') . '</h2>';
+            echo '<p><strong>' . __('User Email:', 'ealicensewoocommerce') . '</strong> ' . esc_html($user_email) . '</p>';
             echo '<p><strong>' . __('License Key', 'ealicensewoocommerce') . ':</strong> ' . esc_html($license_key) . '</p>';
             echo '<p><strong>' . __('Account Limit', 'ealicensewoocommerce') . ':</strong> ' . esc_html($account_quota) . ' Accounts</p>';
             echo '<p><strong>' . __('License Expiration', 'ealicensewoocommerce') . ':</strong> ' . esc_html($license_expiration) . '</p>';
@@ -71,3 +73,84 @@ function ealicensewoocommerce_display_license_info_on_thank_you_page($order_id) 
     }
 }
 add_action('woocommerce_thankyou', 'ealicensewoocommerce_display_license_info_on_thank_you_page');
+
+add_action('woocommerce_thankyou', 'ealicensewoocommerce_create_user_account_after_payment', 10, 1);
+
+function ealicensewoocommerce_create_user_account_after_payment( $order_id ) {
+    // If user is logged in, do nothing because they already have an account
+    if( is_user_logged_in() ) return;
+
+    // Get the newly created order
+    $order = wc_get_order( $order_id );
+
+    // Get the billing email address
+    $order_email = $order->billing_email;
+
+    // Check if there are any users with the billing email as user or email
+    $email = email_exists( $order_email );
+    $user = username_exists( $order_email );
+
+    // Get the order status (see if the customer has paid)
+    $order_status = $order->get_status();
+
+    // Check if the user exists and if the order status is processing or completed (paid)
+    if( $user == false && $email == false && $order->has_status( 'processing' ) || $user == false && $email == false && $order->has_status( 'completed' ) ) {
+        // Check on category ( multiple categories can be entered, separated by a comma )
+
+            // Random password with 12 chars
+            $random_password = wp_generate_password();
+
+            // Firstname
+            $first_name = $order->get_billing_first_name();
+
+            // Lastname
+            $last_name = $order->get_billing_last_name();
+
+            // Role
+            $role = 'customer';
+
+            // Create new user with email as username, newly created password and user role
+            $user_id = wp_insert_user(
+                array(
+                    'user_email' => $order_email,
+                    'user_login' => $order_email,
+                    'user_pass'  => $random_password,
+                    'first_name' => $first_name,
+                    'last_name'  => $last_name,
+                    'role'       => $role,
+                )
+            );
+
+            // (Optional) WC guest customer identification
+            update_user_meta( $user_id, 'guest', 'yes' );
+
+            // User's billing data
+            update_user_meta( $user_id, 'billing_address_1', $order->billing_address_1 );
+            update_user_meta( $user_id, 'billing_address_2', $order->billing_address_2 );
+            update_user_meta( $user_id, 'billing_city', $order->billing_city );
+            update_user_meta( $user_id, 'billing_company', $order->billing_company );
+            update_user_meta( $user_id, 'billing_country', $order->billing_country );
+            update_user_meta( $user_id, 'billing_state', $order->billing_state );
+            update_user_meta( $user_id, 'billing_email', $order->billing_email );
+            update_user_meta( $user_id, 'billing_first_name', $order->billing_first_name );
+            update_user_meta( $user_id, 'billing_last_name', $order->billing_last_name );
+            update_user_meta( $user_id, 'billing_phone', $order->billing_phone );
+            update_user_meta( $user_id, 'billing_postcode', $order->billing_postcode );
+
+            // User's shipping data
+            update_user_meta( $user_id, 'shipping_address_1', $order->shipping_address_1 );
+            update_user_meta( $user_id, 'shipping_address_2', $order->shipping_address_2 );
+            update_user_meta( $user_id, 'shipping_city', $order->shipping_city );
+            update_user_meta( $user_id, 'shipping_company', $order->shipping_company );
+            update_user_meta( $user_id, 'shipping_state', $order->shipping_state );
+            update_user_meta( $user_id, 'shipping_country', $order->shipping_country );
+            update_user_meta( $user_id, 'shipping_first_name', $order->shipping_first_name );
+            update_user_meta( $user_id, 'shipping_last_name', $order->shipping_last_name );
+            update_user_meta( $user_id, 'shipping_method', $order->shipping_method );
+            update_user_meta( $user_id, 'shipping_postcode', $order->shipping_postcode );
+
+            // Link past orders to this newly created customer
+            wc_update_new_customer_past_orders( $user_id );
+
+    }
+}
--- inc/public/ealicensewoocommerce_function_public_woocommerce_email.php
+++ inc/public/ealicensewoocommerce_function_public_woocommerce_email.php
@@ -14,7 +14,7 @@ function ealicensewoocommerce_add_license_info_to_email($order, $sent_to_admin, 
     }
     
     if ($email->id === 'customer_completed_order') {
-        sleep(2);
+        sleep(1);
         $order_id = $order->get_id();
         $download_url = 'https://eastaging.yourrobotrader.com/wp-content/uploads/2024/08/Software_Box_Mockup_robotrader-shadow-web-e1662613546511.png';
 
@@ -23,55 +23,24 @@ function ealicensewoocommerce_add_license_info_to_email($order, $sent_to_admin, 
         $account_quota = get_post_meta($order->get_id(), '_ealicensewoocommerce_account_quota', true);
         $license_expiration = get_post_meta($order->get_id(), '_ealicensewoocommerce_license_expiration', true);
         $user_email = get_post_meta($order->get_id(), '_ealicensewoocommerce_email', true);
-        $user_password = get_post_meta($order->get_id(), '_ealicensewoocommerce_password', true);
-
 
         if ($license_key) {
             if ($plain_text) {
                 // Plain text email format
+                echo "Email: " . $user_email . "\n";
                 echo "License Key: " . $license_key . "\n";
                 echo "Account Limit: " . $account_quota . " Accounts\n";
-                echo "License Expiration: " . $license_expiration . "\n";
-                echo "License Expiration: " . $license_expiration . "\n";
                 echo "License Expiration: " . $license_expiration . "\n";
                 echo "Download your software here: " . esc_url($download_url) . "\n";
             } else {
                 // HTML email format
+                echo '<p><strong>' . __('User Email:', 'ealicensewoocommerce') . '</strong> ' . esc_html($user_email) . '</p>';
                 echo '<p><strong>' . __('License Key:', 'ealicensewoocommerce') . '</strong> ' . esc_html($license_key) . '</p>';
                 echo '<p><strong>' . __('Account Limit:', 'ealicensewoocommerce') . '</strong> ' . esc_html($account_quota) . ' Accounts</p>';
                 echo '<p><strong>' . __('License Expiration:', 'ealicensewoocommerce') . '</strong> ' . esc_html($license_expiration) . '</p>';
-                echo '<p><strong>' . __('User Email:', 'ealicensewoocommerce') . '</strong> ' . esc_html($user_email) . '</p>';
-                echo '<p><strong>' . __('User Password:', 'ealicensewoocommerce') . '</strong> ' . esc_html($user_password) . '</p>';
                 echo '<p><a href="' . esc_url($download_url) . '" target="_blank">' . __('Download your file here', 'ealicensewoocommerce') . '</a></p>';
             }
         }
     }
 }
 add_action('woocommerce_email_order_meta', 'ealicensewoocommerce_add_license_info_to_email', 10, 4);
-
-
-function ealicensewoocommerce_add_license_info_to_admin_email($order, $sent_to_admin, $plain_text, $email) {
-    if (!ealicensewoocommerce_is_license_enabled()) {
-        return; // Exit if the feature is not enabled
-    }
-    
-    if ($sent_to_admin && $email->id === 'customer_completed_order') {
-        $order_id = $order->get_id();
-        $account_id = get_post_meta($order_id, '_yrt_license_account_number', true);
-        $license_key = get_post_meta($order_id, '_yrt_license_license_key', true);
-        $download_url = 'https://eastaging.yourrobotrader.com/wp-content/uploads/2024/08/Software_Box_Mockup_robotrader-shadow-web-e1662613546511.png';
-
-        if ($plain_text) {
-            echo "Account ID: " . $account_id . "\n";
-            echo "License Key: " . $license_key . "\n";
-            echo "Download your file here: " . $download_url . "\n";
-        } else {
-            echo '<h3>' . __('Your License Details') . '</h3>';
-            echo '<p><strong>' . __('Account ID') . ':</strong> ' . esc_html($account_id) . '</p>';
-            echo '<p><strong>' . __('License Key') . ':</strong> ' . esc_html($license_key) . '</p>';
-            echo '<p><a href="' . esc_url($download_url) . '" target="_blank">' . __('Download your file here') . '</a></p>';
-        }
-        
-    }
-}
-add_action('woocommerce_email_order_meta', 'ealicensewoocommerce_add_license_info_to_admin_email', 20, 4);
