71c5971e29927d60397f5ee47b876b134a7f5c9c
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-09-12 14:47:48 +0800

    Update ealicensewoocommerce_function_public_woocommerce_email_new_users.php

--- inc/public/ealicensewoocommerce_function_public_woocommerce_email_new_users.php
+++ inc/public/ealicensewoocommerce_function_public_woocommerce_email_new_users.php
@@ -24,8 +24,8 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 <?php /* translators: %1$s: Site title, %2$s: Username, %3$s: My account link */ ?>
 <p><?php printf( esc_html__( 'Thanks for creating an account on %1$s. Your username is %2$s. You can access your account area to view orders, change your password, and more at: %3$s', 'woocommerce' ), esc_html( $blogname ), '<strong>' . esc_html( $user_login ) . '</strong>', make_clickable( esc_url( wc_get_page_permalink( 'myaccount' ) ) ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 <?php if ( 'yes' === get_option( 'woocommerce_registration_generate_password' ) && $password_generated && $set_password_url ) : ?>
-	<?php // If the password has not been set by the user during the sign up process, send them a link to set a new password ?>
-	<p><a href="<?php echo esc_attr( $set_password_url ); ?>"><?php printf( esc_html__( 'Click here to set your new password.', 'woocommerce' ) ); ?></a></p>
+    <?php // If the password has not been set by the user during the sign up process, send them a link to set a new password ?>
+    <p><a href="<?php echo esc_attr( $set_password_url ); ?>"><?php printf( esc_html__( 'Click here to set your new password.', 'woocommerce' ) ); ?></a></p>
 <?php endif; ?>
 
 <?php if ( isset( $user_pass ) ) : ?>
@@ -34,15 +34,11 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
     </p>
     <p>
         <?php _e( 'Username:', 'woocommerce' ); ?>
-        <span style="background-color: #f4f4f4; padding: 5px 10px; display: inline-block; margin-top: 10px; font-weight: bold; color: #444;"><?php echo esc_html( $user_login ); ?></span>
-    </p>
-    <p>
+        <span><?php echo esc_html( $user_login ); ?></span><br>
         <?php _e( 'Password:', 'woocommerce' ); ?>
-        <span style="background-color: #f4f4f4; padding: 5px 10px; display: inline-block; margin-top: 10px; font-weight: bold; color: #444;"><?php echo esc_html( $user_pass ); ?></span>
-    </p>
-    <p>
+        <span><?php echo esc_html( $user_pass ); ?></span><br>
         <?php _e( 'Account Login:', 'woocommerce' ); ?>
-        <a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" style="background-color: #f4f4f4; padding: 5px 10px; display: inline-block; margin-top: 10px; font-weight: bold; color: #444;">
+        <a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>">
             <?php _e( 'Click here to login', 'woocommerce' ); ?>
         </a>
     </p>
@@ -55,7 +51,7 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
  * Show user-defined additional content - this is set in each email's settings.
  */
 if ( $additional_content ) {
-	echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
+    echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
 }
 
 do_action( 'woocommerce_email_footer', $email );
