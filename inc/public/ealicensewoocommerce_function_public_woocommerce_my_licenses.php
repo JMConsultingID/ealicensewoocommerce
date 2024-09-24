055ec73905d443d9a02bc09d43d4492e516f878e
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-09-24 14:13:25 +0800

    Update ealicensewoocommerce_function_public_woocommerce_my_licenses.php

--- inc/public/ealicensewoocommerce_function_public_woocommerce_my_licenses.php
+++ inc/public/ealicensewoocommerce_function_public_woocommerce_my_licenses.php
@@ -12,10 +12,10 @@ function ealicensewoocommerce_menu_items($items) {
     // Unset Address Menu 
     unset($items['edit-address']); 
     // Insert the new menu items after the "Orders" tab (or wherever you want them)
-    $new_items = array_slice($items, 0, 1, true) +
+    $new_items = array_slice($items, 0, 2, true) +
                  array('my-licenses' => __('My License', 'ealicensewoocommerce')) +
                  array('video-tutorials' => __('Video Tutorials', 'ealicensewoocommerce')) +
-                 array_slice($items, 1, null, true);
+                 array_slice($items, 2, null, true);
     return $new_items;
 }
 add_filter('woocommerce_account_menu_items', 'ealicensewoocommerce_menu_items');
