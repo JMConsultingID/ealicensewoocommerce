82e3f380cec541f32098aad04f9add1c24c60667
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-10-03 15:00:05 +0800

    update core

--- inc/ealicensewoocommerce_functions.php
+++ inc/ealicensewoocommerce_functions.php
@@ -18,7 +18,7 @@ require dirname(__FILE__) . '/helper/ealicensewoocommerce_function_helper.php';
 require dirname(__FILE__) . '/public/ealicensewoocommerce_function_public_woocommerce.php';
 require dirname(__FILE__) . '/public/ealicensewoocommerce_function_public_woocommerce_api.php';
 require dirname(__FILE__) . '/public/ealicensewoocommerce_function_public_woocommerce_email.php';
-require dirname(__FILE__) . '/public/ealicensewoocommerce_function_public_woocommerce_my_account_menu_additional.php';
+require dirname(__FILE__) . '/public/ealicensewoocommerce_function_public_woocommerce_my_account_menu.php';
 
 function ealicensewoocommerce_enqueue_admin_assets($hook_suffix) {
     // Register the CSS file
