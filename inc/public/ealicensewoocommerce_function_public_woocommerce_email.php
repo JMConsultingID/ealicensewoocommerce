8ef88e8c8577cfe6f74c537af6e12d729585c3de
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-10-11 15:13:22 +0800

    Update ealicensewoocommerce_function_public_woocommerce_email.php

--- inc/public/ealicensewoocommerce_function_public_woocommerce_email.php
+++ inc/public/ealicensewoocommerce_function_public_woocommerce_email.php
@@ -16,7 +16,6 @@ function ealicensewoocommerce_add_license_info_to_email($order, $sent_to_admin, 
     if ($email->id === 'customer_completed_order') {
         sleep(2);
         $order_id = $order->get_id();
-        $download_url = 'https://eastaging.yourrobotrader.com/wp-content/uploads/2024/08/Software_Box_Mockup_robotrader-shadow-web-e1662613546511.png';
 
         // Retrieve the license key from order meta
         $license_key = get_post_meta($order->get_id(), '_ealicensewoocommerce_license_key', true);
@@ -31,14 +30,12 @@ function ealicensewoocommerce_add_license_info_to_email($order, $sent_to_admin, 
                 echo "License Key: " . $license_key . "\n";
                 echo "Account Limit: " . $account_quota . " Accounts\n";
                 echo "License Expiration: " . $license_expiration . "\n";
-                echo "Download your software here: " . esc_url($download_url) . "\n";
             } else {
                 // HTML email format
                 echo '<p><strong>' . __('User Email:', 'ealicensewoocommerce') . '</strong> ' . esc_html($user_email) . '</p>';
                 echo '<p><strong>' . __('License Key:', 'ealicensewoocommerce') . '</strong> ' . esc_html($license_key) . '</p>';
                 echo '<p><strong>' . __('Account Limit:', 'ealicensewoocommerce') . '</strong> ' . esc_html($account_quota) . ' Accounts</p>';
                 echo '<p><strong>' . __('License Expiration:', 'ealicensewoocommerce') . '</strong> ' . esc_html($license_expiration) . '</p>';
-                echo '<p><a href="' . esc_url($download_url) . '" target="_blank">' . __('Download your file here', 'ealicensewoocommerce') . '</a></p>';
             }
         }
     }
